3d3c397fb1beb8ec98e3e36b049747fa8e08f395
JaribK <mewn7788@gmail.com>
2024-10-04 22:58:35 +0700

    update billboradcheck

--- maechan/www/billboardcheck.py
+++ maechan/www/billboardcheck.py
@@ -9,7 +9,7 @@ def get_context(context):
         # Fetch all parent data for the specified owner_cid
         parent_data = frappe.get_all('Billboard Document2',
                                       filters={'owner_cid': owner_cid},
-                                      fields=['name', 'owner_name', 'total_price', 'no_receipt', 'research_by'])
+                                      fields=['name', "land_id", 'owner_name', 'total_price', 'no_receipt', 'research_by'])
 
         if parent_data:
             context['results'] = []  # Initialize a list to store results
@@ -18,24 +18,20 @@ def get_context(context):
             for parent in parent_data:
                 parent_name = parent['name']
                 
-                land_data = frappe.get_all('Billboard Land2',
-                                            filters={'parent': parent_name},
-                                            fields=['no', 'road', 'moo', 'soi', 'sub_district', 'district', 'province', 'postal_code', 'latitude', 'longitude'])
-                
                 billboard_data = frappe.get_all('Billboard Data2',
                                                  filters={'parent': parent_name},
-                                                 fields=['picture', 'width', 'height', 'type_of_billboards', 'price'])
+                                                 fields=['picture', 'width', 'height', 'type_of_billboards', 'price','billboard_status'])
 
                 billboard_count = len(billboard_data)
 
                 # Append results to context
                 context['results'].append({
                     'owner_name': parent['owner_name'],
-                    'land_id':parent['name'],
-                    'land_data': land_data,
+                    'land_id':parent['land_id'],
                     'billboard_count': billboard_count,
                     'data_billboards': billboard_data,
                     'total_price': parent.get('total_price'),
+                    'test':parent_data
                 })
 
             context['show_results'] = True
